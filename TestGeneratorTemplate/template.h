b95fe605ae8e2b08cd2a8d218236a5c936a5710c
NguyenHuuNhatQuang <119930189+NguyenHuuNhatQuang@users.noreply.github.com>
2024-09-07 21:23:52 +0700

    Update template.h
    Update tính năng sinh cây và thêm tính năng sinh dãy ngoặc đúng.

--- TestGeneratorTemplate/template.h
+++ TestGeneratorTemplate/template.h
@@ -54,32 +54,10 @@ string bignum(long long l) // sinh mot so lon gom l chu so
     return s;
 }
 
-vector<pair<int, int> > tree(int ver) // sinh mot cay va tra ve cac canh
+vector <pair <int, int>> tree (int ver, int d) // tra ve cac canh cua mot cay co do sau lon
 {
-    assert (ver > 0);
-
-    vector<pair<int, int> > ans;
-    for(int i = 2; i <= ver; i ++)
-        ans.push_back(make_pair(i, Rand(1, i - 1)));
-
-    for(int i = 1; i < ver - 1; i ++) swap(ans[i], ans[Rand(0, i)]);
-
-    int secret = 0;
-    while(__gcd(secret, ver) != 1) secret = Rand(1, ver);
-    for(int i = 0; i < ver - 1; i ++)
-    {
-        ans[i].first = 1LL * ans[i].first * secret % ver;
-        if(ans[i].first == 0) ans[i].first = ver;
-        ans[i].second = 1LL * ans[i].second * secret % ver;
-        if(ans[i].second == 0) ans[i].second = ver;
-    }
-
-    return ans;
-}
-
-vector <pair <int, int> > high_depth_tree (int ver, int d) // tra ve cac canh cua mot cay co do sau lon
-{
-    assert (ver > 0 && d > 0);
+    assert (ver > 0 && d >= 0);
+    if (d == 0) d = ver;
 
     vector<pair<int, int> > ans;
     for(int i = 2; i <= ver; i ++)
@@ -134,18 +112,19 @@ vector<pair<int, int> > graph(int ver, int edge) // tra ve cac canh cua mot do t
     return ans;
 }
 
-vector<pair<int, int> > connected_graph (int ver, int edge) // tra ve cac canh cua mot do thi lien thong
+vector<pair<int, int>> connected_graph (int ver, int edge) // tra ve cac canh cua mot do thi lien thong
 {
-    assert (ver > 0 && edge >= ver - 1);
+    assert (ver > 0 && edge >= ver - 1 && edge <= ver * (ver - 1) / 2);
 
     unordered_map<long long, bool> used;
     used[0] = true;
 
-    vector<pair<int, int> > ans;
-    for(int i = 2; i <= ver; i ++)
+    vector<pair<int, int>> ans = tree (ver, 0);
+
+    for (auto i: ans)
     {
-        ans.push_back(make_pair(i, Rand(1, i - 1)));
-        used[1LL * ans.back().first * ver + ans.back().second] = true;
+        if (i.first < i.second) swap (i.first, i.second);
+        used[1LL * i.first * ver + i.second] = true;
     }
 
     for(int i = ver; i <= edge; i ++)
@@ -182,4 +161,43 @@ long long log_rand(long long t) // sinh mot so ngau nhien tu 1 den t
 
     int x = 64 - __builtin_clzll(t);
     return Rand(1, min(t, (1LL << Rand(1, x))));
+}
+
+string bracket (int n, int d) // tra ve mot day ngoac dung
+{
+    assert (n > 0 and d >= 0);
+
+    vector <pair <int, int>> edges = tree (n, d);
+    vector <int> adj[n + 1], st;
+    string ans;
+
+    for (auto i: edges)
+    {
+        adj[i.first].push_back (i.second);
+        adj[i.second].push_back (i.first);
+    }
+
+    st.push_back (0);
+    st.push_back (1);
+
+    while (int (st.size ()) > 1)
+    {
+        int t = st.back ();
+        if (adj[t].size ())
+        {
+            if (adj[t].back () != st[st.size () - 2])
+            {
+                ans += '(';
+                st.push_back (adj[t].back ());
+            }
+            adj[t].pop_back ();
+        }
+        else
+        {
+            ans += ')';
+            st.pop_back ();
+        }
+    }
+
+    return ans;
 }
